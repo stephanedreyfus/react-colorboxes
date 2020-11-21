9bb62d457f8729e8b7cb8b4a7045e028e02f996a
Stéphane Dreyfus <floatingrock@gmail.com>
2020-11-20 19:03:43 -0800

    Adding basic board and box component files.

---
+++ src/Board.js
@@ -0,0 +1,13 @@
+// Element that will hold all the color squares.
+import React from "react";
+import ColorBox from "./ColorBox";
+
+const Board = () => {
+  return (
+    <section>
+      {/* map boxes here */}
+    </section>
+  )
+}
+
+export default Board;
